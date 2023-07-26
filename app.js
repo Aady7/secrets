50bfbf0f5492d5315b3d1d3d11ea807454669745
Aady <aditya.psinghciv22@iitbhu.ac.in>
2023-07-26 17:12:13 +0530

    my first commit

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
+require('dotenv').config()
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const app=express();
 const mongoose=require("mongoose");
+const { stringify } = require("querystring");
+const encrypt= require("mongoose-encryption");
 app.use(bodyParser.urlencoded({
     extended:true
 }));
+const dbURL = 'mongodb://127.0.0.1:27017/userDB'
 app.set('view engine','ejs');
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true})
 
-const userSchema={
-    
+
+setTimeout(() => {
+    mongoose.connect(dbURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+        .then(() => {
+            console.log('Connected to the database!');
+        })
+        .catch((err) => {
+            console.error('Error connecting to the database:', err);
+        });
+}, 5000); // Wait 5 seconds before connecting
+
+
+const userSchema=new mongoose.Schema({
     email:String,
     password:String
-    
-}
-const user= new mongoose.model("user", userSchema)
+});
+
+
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:["password"]});
+
+const User=mongoose.model("User", userSchema);
+
 app.get("/", function(request,response){
     response.render("home");
 });
@@ -28,12 +49,44 @@ app.get("/register", function(request, response){
     response.render("register");
 
 })
-
 app.post("/register", function(req,res){
-    
+    const newUser= new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+    newUser.save()
+    .then(()=>{
+       res.render("secrets")
+    })
+    .catch((err)=>{
+        console.log(err)
+    });
 
 
 })
+
+app.post("/login", function(req,res){
+   username=req.body.username;
+    password=req.body.password;
+User.findOne({email:username})
+.then((founduser)=>{
+    if(founduser.password===password){
+        res.render("secrets")
+    }
+    else{
+        res.render("login")
+    }
+})
+.catch((err)=>{
+    console.log(err)
+})
+    
+})
+
+
+
+
+
 
 app.listen(3000, function(){
     console.log("Listening to port 3000");
