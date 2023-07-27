bf46a9fe82e0f15d2b3df3661794a4721f4b42ca
Aady <aditya.psinghciv22@iitbhu.ac.in>
2023-07-27 16:42:43 +0530

    Fifth commit

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 require('dotenv').config()
 var md5 = require('md5');
-const express=require("express");
-const bodyParser=require("body-parser");
-const ejs=require("ejs");
-const app=express();
-const mongoose=require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const mongoose = require("mongoose");
 const { stringify } = require("querystring");
-const bcrypt = require('bcrypt');
+
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const { userInfo } = require('os');
+const dbURL = 'mongodb://127.0.0.1:27017/userDB'
 
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }));
-const dbURL = 'mongodb://127.0.0.1:27017/userDB'
-app.set('view engine','ejs');
-app.use(express.static("public"));
 
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(session({
+    secret: "I will crack gcoc2024",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 setTimeout(() => {
     mongoose.connect(dbURL, {
@@ -30,59 +42,95 @@ setTimeout(() => {
 }, 5000); // Wait 5 seconds before connecting
 
 
-const userSchema=new mongoose.Schema({
-    email:String,
-    password:String
+const userSchema = new mongoose.Schema({
+   username: String,
+    password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+const User = mongoose.model("User", userSchema);
+
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
-const User=mongoose.model("User", userSchema);
-
-app.get("/", function(request,response){
+app.get("/", function (request, response) {
     response.render("home");
 });
-app.get("/login", function(request,response){
+app.get("/login", function (request, response) {
     response.render("login");
 
 })
-app.get("/register", function(request, response){
+app.get("/register", function (request, response) {
     response.render("register");
 
 })
-app.post("/register", function(req,res){
-    const newUser= new User({
-        email:req.body.username,
-        password:md5(req.body.password)
-    });
-    newUser.save()
-    .then(()=>{
-       res.render("secrets")
-    })
-    .catch((err)=>{
-        console.log(err)
-    });
 
 
-})
-
-app.post("/login", function(req,res){
-   username=req.body.username;
-    password=md5(req.body.password);
-User.findOne({email:username})
-.then((founduser)=>{
-    if(founduser.password===password){
-        res.render("secrets")
+app.get("/secrets", function(request,response){
+    if(request.isAuthenticated()){
+        response.render("secrets")
     }
     else{
-        res.render("login")
+        response.render("login");
     }
 })
-.catch((err)=>{
-    console.log(err)
-})
+
+
+app.get("/logout", function(req,res){
+   req.logOut(function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        res.redirect("/");
+    }
+   })
     
+
+})
+app.post("/register", function (req, res) {
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if(err) {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            })
+
+        }
+    })
+
+
+
+})
+
+app.post("/login", function (req, res) {
+    const user=new User({
+        username:req.body.username,
+        password:req.body.password
+
+    });
+   
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets");
+            })
+        }
+    })
+
+
+
 })
 
 
@@ -90,7 +138,7 @@ User.findOne({email:username})
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Listening to port 3000");
 })
 
